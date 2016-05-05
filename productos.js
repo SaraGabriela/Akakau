d443da2831b9594edacc558fcb218a537ea61e53
Shirley <sharylizzi24@gmail.com>
2016-05-05 15:37:56 -0500

    avance

--- productos.js
+++ productos.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $("#carta").accordion({collapsible: true, active: false});
-});
+var visto = null;
+function mostrar(num) {
+  obj = document.getElementById(num);
+  obj.style.display = (obj==visto) ? 'none' : 'block';
+  if (visto != null)
+    visto.style.display = 'none';
+  visto = (obj==visto) ? null : obj;
+}
