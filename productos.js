5fe176249a182b4c8e9ef354cd301a4557ff9dd0
Sara Diaz Oporto <Sara Diaz Oporto>
2016-05-06 07:26:12 -0500

    Ya

--- productos.js
+++ productos.js
@@ -1,8 +1,3 @@
-var visto = null;
-function mostrar(num) {
-  obj = document.getElementById(num);
-  obj.style.display = (obj==visto) ? 'none' : 'block';
-  if (visto != null)
-    visto.style.display = 'none';
-  visto = (obj==visto) ? null : obj;
-}
+$(document).ready(function() {
+    $("#carta").accordion({collapsible: true, active: false});
+});
